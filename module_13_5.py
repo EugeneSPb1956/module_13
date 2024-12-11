97d4870871f8b60365fcbd19c94262d3e0d5604b
EugeneSPb <epoltavets@gmail.com>
2024-12-11 11:18:25 +0300

    Telegram-bot: Клавиатура кнопок

---
+++ module_13_5.py
@@ -0,0 +1,77 @@
+# Домашнее задание по теме "Клавиатура кнопок".
+# Задача "Меньше текста, больше кликов"
+
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+api = ''  # api ключ
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# Клавиатура
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+
+kb.add(button1)
+kb.add(button2)
+
+
+# Обработчик клавматуры
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет!', reply_markup=kb)
+
+
+# класс, описывающий состояние пользователя
+class UserState(StatesGroup):
+    age = State()
+    height = State()
+    weight = State()
+
+
+# Обработчик кнопки
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост в см:')
+    await UserState.height.set()
+
+
+@dp.message_handler(state=UserState.height)
+async def set_weight(message, state):
+    await state.update_data(height=message.text)
+    await message.answer('Введите свой вес в кг:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    calories = 10 * float(data['weight']) + 6.25 * float(data['height']) - 5 * float(data['age']) + 5
+    await message.answer(f'Ваша норма калорий {calories:.2f}')
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@ dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
